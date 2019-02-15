24b4d01e693e7c3278c2bdb80de7245da1964f11
Kyksi <47677914+Kyksi@users.noreply.github.com>
2019-02-15 23:34:57 +0100

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Button" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </div>
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:DropDownList ID="DropDownList2"
+            runat="server">
+        </asp:DropDownList> Zamień na 
+        <asp:DropDownList ID="DropDownList3" runat="server">
+        </asp:DropDownList> 
+        <asp:Button ID="Button2" runat="server" Text="Button" onclick="Button2_Click" />
+    </div>
+    <div>
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Xml;
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            pogoda.GlobalWeather gw = new pogoda.GlobalWeather();
+            string cities = gw.GetCitiesByCountry("Poland");
+            XmlDocument x = new XmlDocument();
+            x.LoadXml(cities);
+            XmlNodeList list = x.GetElementsByTagName("City");
+            foreach (XmlNode xn in list)
+            {
+                ListItem li = new ListItem(xn.InnerText, xn.InnerText);
+                DropDownList1.Items.Add(li);
+            }
+
+            kantor.CurrencyConvertor c = new kantor.CurrencyConvertor();
+            int i=0;
+            foreach (string item in Enum.GetNames(typeof(kantor.Currency)))
+            {
+               ListItem li = new ListItem(item, i.ToString());
+               DropDownList2.Items.Add(li);
+               DropDownList3.Items.Add(li);
+               i++;
+            }
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        pogoda.GlobalWeather gw = new pogoda.GlobalWeather();
+        Label1.Text = gw.GetWeather(DropDownList1.SelectedItem.Text, "Poland");
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        kantor.CurrencyConvertor c = new kantor.CurrencyConvertor();
+        double a = c.ConversionRate((kantor.Currency)Convert.ToInt32(DropDownList2.SelectedValue), (kantor.Currency)Convert.ToInt32(DropDownList3.SelectedValue));
+        Label2.Text = (a*Convert.ToDouble(TextBox1.Text)).ToString();
+    }
+}
